3f43ebf584454104383b7f7e942a2ad33573d742
Lee Nayeon <155801470+y-eonee@users.noreply.github.com>
2024-12-22 16:51:46 +0900

    Update 추억 점수.java

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = {};
+        Map<String, Integer> yearningMap = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            yearningMap.put(name[i], yearning[i]);
+        }
+        
+        int[] answer = new int[photo.length];
+        
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (String person : photo[i]) {
+                sum += yearningMap.getOrDefault(person, 0);
+            }
+            answer[i] = sum;
+        }
+        
         return answer;
     }
 }
